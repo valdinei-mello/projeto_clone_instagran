d986424f01371c34a47c3aa10148bacc625e8bd5
Valdiei de Mello <valdinei.mello.22@gmail.com>
2022-06-01 21:12:07 -0300

    criando instagram clone

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AcessoComponent } from './acesso/acesso.component';
+import { BannerComponent } from './acesso/banner/banner.component';
+import { LoginComponent } from './acesso/login/login.component';
+import { CadastroComponent } from './acesso/cadastro/cadastro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AcessoComponent,
+    BannerComponent,
+    LoginComponent,
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
