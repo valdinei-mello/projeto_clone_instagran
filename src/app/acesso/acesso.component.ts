7ab8bc52f2a6e68c54573aa9da60b38b5e3ce5be
Valdiei de Mello <valdinei.mello.22@gmail.com>
2022-06-08 20:02:15 -0300

    estado especial void e estilos na transição de estados

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,15 +1,30 @@
+import { trigger, state, transition, animate, style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(-50px,0px)' }),
+        animate("500ms 0s ease-in-out")])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
 
   constructor() { }
 
+
+  public estadoCriado: string = 'criado';
+
   ngOnInit(): void {
   }
+
 
 }
