82609e176379bf2a247f143e2d0df318de6372f1
Valdiei de Mello <valdinei.mello.22@gmail.com>
2022-06-13 20:38:27 -0300

    keyframes

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,5 +1,6 @@
-import { trigger, state, transition, animate, style } from '@angular/animations';
+import { trigger, state, transition, animate, style, keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { offset } from '@popperjs/core';
 
 @Component({
   selector: 'app-acesso',
@@ -12,7 +13,8 @@ import { Component, OnInit } from '@angular/core';
       })),
       transition('void => criado', [
         style({ opacity: 0, transform: 'translate(-50px,0px)' }),
-        animate("500ms 0s ease-in-out")])
+
+        animate("500ms 0s ease-in-out")]),
     ]),
     trigger('animacao-painel', [
       state('criado', style({
@@ -20,7 +22,20 @@ import { Component, OnInit } from '@angular/core';
       })),
       transition('void=>criado', [
         style({ opacity: 0, transform: 'translate(50px,0px)' }),
-        animate("500ms 0s ease-in-out")])
+        animate("1.5s 0s ease-in-out", keyframes([
+          style({ offset: 0.15, opacity: 1, transform: 'translateX(0)' }),
+          style({ offset: 0.86, opacity: 1, transform: 'translateX(0)' }),
+
+          style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.90, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 0.92, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.94, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 0.96, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.98, opacity: 1, transform: 'translateY(10px)' })
+
+
+        ]))])
+
     ]
     )
   ]
