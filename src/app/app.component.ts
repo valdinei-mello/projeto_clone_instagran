a4362ed837085229af7fd51d02909df0cab6df69
Valdiei de Mello <valdinei.mello.22@gmail.com>
2022-06-21 20:00:27 -0300

    firebase with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app3';
+
+  ngOnInit(): void {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyCXGDKvaifpA8shm93HsRyP2zKyse0tYyk",
+      authDomain: "jts-instagram-clone-34f51.firebaseapp.com",
+      databaseURL:"https://jts-instagram-clone-34f51-default-rtdb.firebaseio.com",
+      projectId: "jts-instagram-clone-34f51",
+      storageBucket: "jts-instagram-clone-34f51.appspot.com",
+      messagingSenderId: "43239335190",
+      appId: "1:43239335190:web:2e2e8e5e0330abef25dc37",
+      measurementId: "G-V8VSV2WG3L"
+    };
+
+    firebase.initializeApp(firebaseConfig)
+  }
 }
--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,7 +1,22 @@
 import { Usuario } from "./acesso/usuario.model";
+import * as firebase from "firebase";
 
 export class Autenticacao {
     public cadastrarUsuario(usuario: Usuario): void {
-        console.log('chegamos aqqui', usuario)
+        //console.log('chegamos aqqui', usuario)
+
+        firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
+            .then((resposta: any) => {
+
+                //remove a senha do usuario e nao salva no banco de dados
+               // delete usuario.email
+                    
+                //registrando dados do usuario no path email na vase 64
+                firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+                    .set(usuario)
+            })
+            .catch((error: Error) => {
+                console.log(error)
+            })
     }
 }
